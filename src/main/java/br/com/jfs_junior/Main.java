a79b2a439220e6a78e472662d185b5029df53c88
Josemilson Junior <junior.tn1@gmail.com>
2023-03-12 13:52:52 -0300

    Interacoes com Lista implementacao metodo comparable

--- src/main/java/br/com/jfs_junior/Main.java
+++ src/main/java/br/com/jfs_junior/Main.java
@@ -1,7 +1,25 @@
 package br.com.jfs_junior;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Gato> meusGatos = new ArrayList<>() {{
+           add(new Gato("Spock", 116, "Branco"));
+           add(new Gato("Uhura", 26, "Preto"));
+           add(new Gato("Data", 2, "Tigrado"));
+        }};
+        System.out.println("[[[ ORDEM DE INSERCAOO ]]]");
+        System.out.println(meusGatos);
+
+        System.out.println("[[[ ORDEM ALEATORIAO ]]]");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
+
+        System.out.println("[[[ ORDEM NATURAL ]]]");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
     }
 }
